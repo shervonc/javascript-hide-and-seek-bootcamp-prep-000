7a0ae5bc2ced8326202048d5c7859a347cb5a874
Von C <shervonc@gmail.com>
2020-01-17 15:11:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector(` #nested .target` )
 }
 function increaseRankBy(n) {
-  const List = document.querySelector("ul ranked-list li")
+  const List = document.querySelectorAll("ul ranked-list li")
   for (let i=0, i<n;i++) {
   List[i] = parseInt(List[i]) + 1
   }
