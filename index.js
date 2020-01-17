1aab03b1a57a50e75966d9bf44c7c6946eefdc28
Von C <shervonc@gmail.com>
2020-01-17 15:21:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ function nestedTarget() {
   return document.querySelector(` #nested .target` )
 }
 function increaseRankBy(n) {
-  const List = document.querySelectorAll(" .ranked-list")
-  for (let i=0, i<list.length;i++) {
-  List[i] = parseInt(List[i]) + 1
+   const RList = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, x = RList.length; i < x; i++) {
+    let child = RList[i].children
+
+    for (let a = 0, b = child.length; a < b; a++) {
+      child[a].innerHTML = parseInt(child[a].innerHTML) + n
+    }
   }
-}
+} 
