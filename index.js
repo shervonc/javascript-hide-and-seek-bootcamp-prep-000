13b4da6d8efe0e5e397f55e621381b25b46c47cf
Von C <shervonc@gmail.com>
2020-01-17 17:05:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector(` #nested .target` )
+}
+function increaseRankBy(n) {
+  const Rlist = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < Rlist.length; i++) {
+      Rlist[i].innerHTML = parseInt(Rlist[i].innerHTML) + n;
+    }
+  }
+  function deepestChild(){
+  var master = document.getElementById('grand-node')
+  var child = master.children[0]
+  
+  while (child){
+    master = child
+    child = master.children[0];
+  }
+  return master;
+}
+    
+    
+  
+  
